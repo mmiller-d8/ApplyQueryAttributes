351144a331c0374a110b79d5c22efa2060be4231
mmiller-d8 <142229826+mmiller-d8@users.noreply.github.com>
2024-05-20 08:06:08 -0500

    Initial project creation

---
+++ ApplyQueryAttributesTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ApplyQueryAttributesTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
